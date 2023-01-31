6b6d0b6138e42cf1491b26117c1a11b91916ecc5
PadraicHeberlein <padraic.heb@gmail.com>
2023-01-31 17:26:01 -0500

    Successfully tested exp() in TestQuaterion.java..

--- TestQuaternion.java
+++ TestQuaternion.java
@@ -2,6 +2,9 @@ public class TestQuaternion
 {
 	public static void main(String[] args)
 	{
-		
+		Quaternion q = new Quaternion(1,2,3,4);
+
+		System.out.println("q = " + q);
+		System.out.println("exp(q) = " + Quaternion.exp(q));		
 	}
 }
