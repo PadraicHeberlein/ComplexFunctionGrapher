ac67d1f32f753136c8ca7e08c353433c50afca90
PadraicHeberlein <padraic.heb@gmail.com>
2023-02-05 12:37:17 -0500

    Added methods for mult by scalar and dot and cross products.

--- VectorR3.java
+++ VectorR3.java
@@ -9,38 +9,61 @@ public class VectorR3 extends Quaternion
 	
 	public VectorR3(double x, double y, double z) { super(0.0, x, y, z); } 
 
-	public VectorR3(VectorR3 other)
+	public VectorR3(VectorR3 v)
 	{
-		set(Quaternion.B, other.get(Quaternion.B));
-		set(Quaternion.C, other.get(Quaternion.C));
-		set(Quaternion.D, other.get(Quaternion.D));
+		set(Quaternion.B, v.get(Quaternion.B));
+		set(Quaternion.C, v.get(Quaternion.C));
+		set(Quaternion.D, v.get(Quaternion.D));
 	}
 	
-	public VectorR3(Quaternion other) 
+	public VectorR3(Quaternion q) 
 	{ 
-		super(other);
+		super(q);
 		set(Quaternion.A, 0.0); 
 	}
 
 	public double get(int i) { return super.get(i + 1); }
 
-	public VectorR3 add(VectorR3 other)
+	public void set(int i, double value) { super.set(i + 1, value); }
+
+	public VectorR3 add(VectorR3 v)
 	{
 		Quaternion q_1 = new Quaternion(this);
-		Quaternion q_2 = new Quaternion(other);
+		Quaternion q_2 = new Quaternion(v);
 	
 		return new VectorR3(q_1.add(q_2));
 	}
 	
+  	public VectorR3 neg() { return new VectorR3(-1*get(X), -1*get(Y), -1*get(Z)); }
+
+	public VectorR3 sub(VectorR3 v) { return add(v.neg()); }
+
+	// multiplication by a scalar value
+	public VectorR3 x(double s) { return new VectorR3(s*get(X), s*get(Y), s*get(Z)); }
 	
+	// scalar product
+	public double dot(VectorR3 v) 
+	{ 
+		return get(X)*v.get(X) + get(Y)*v.get(Y) + get(Z)* v.get(Z); 
+	}
+
+	public double sX(VectorR3 other) { return dot(other); }
+
+	// cross product / vector product
+	public VectorR3 cross(VectorR3 other)
+	{
+		double newXpart = get(Y)*other.get(Z) - get(Z)*other.get(Y);
+		double newYpart = get(Z)*other.get(X) - get(X)*other.get(Z);
+		double newZpart = get(X)*other.get(Y) - get(Y)*other.get(X);
+
+		return new VectorR3(newXpart, newYpart, newZpart);
+	}
+
+	public VectorR3 vX(VectorR3 other) { return cross(other); }
 
 	@Override
 	public String toString()
 	{
-		double x = get(X);
-		double y = get(Y);
-		double z = get(Z);
-
-		return "(" + x + ", " + y + ", " + z + ")";
+		return "(" + get(X) + ", " + get(Y) + ", " + get(Z) + ")";
 	}
 }
