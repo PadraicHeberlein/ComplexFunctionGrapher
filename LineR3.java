167710d5ca03ccdce8b5b424b331b7ae8e6bdabc
PadraicHeberlein <padraic.heb@gmail.com>
2023-02-06 18:22:23 -0500

    Updated LineR3 and ShapeR3.

--- LineR3.java
+++ LineR3.java
@@ -1,4 +1,8 @@
 public class LineR3
-{	
+{
+	private VectorR3 line;
+	private VectorR3 start;
+	private VectorR3 end;
+	
 	public LineR3() { }
 }
--- ShapeR3.java
+++ ShapeR3.java
@@ -4,9 +4,9 @@ public interface ShapeR3
 {
 	public VectorR3 centerOfMass();
 
-	public boolean intersectsWith(LineR3 ray);
+	public boolean intersectsWith(VectorR3 ray);
 
-	public LineR3 reflect(LineR3 ray);
+	public VectorR3 reflect(VectorR3 ray);
 
 	public VectorR3 normalAt(VectorR3 onSurface);
 
