65e1547f511760635156b7d81625baab790f5010
PadraicHeberlein <padraic.heb@gmail.com>
2023-02-06 17:55:45 -0500

    Turned ShapeR3 into a public interface.

--- ShapeR3.java
+++ ShapeR3.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 public interface ShapeR3
 {
 	public VectorR3 centerOfMass();
@@ -5,4 +7,10 @@ public interface ShapeR3
 	public boolean intersectsWith(LineR3 ray);
 
 	public LineR3 reflect(LineR3 ray);
+
+	public VectorR3 normalAt(VectorR3 onSurface);
+
+	public VectorR3 intersection(LineR3 ray);
+
+	public Color colorAt(VectorR3 onSurface);
 }
